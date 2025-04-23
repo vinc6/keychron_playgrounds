97063fc143e949fb4b2c5a6a33736711d219d9f7
vinc kwok <vinc@vincs-MacBook-Air.local>
2025-04-23 21:26:53 +0800

    Swap position for WIN and ALT keys, and add Stop key on Fn layer

--- keyboards/keychron/q60_max/ansi/keymaps/vinc6/keymap.c
+++ keyboards/keychron/q60_max/ansi/keymaps/vinc6/keymap.c
@@ -49,14 +49,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, BT_HST1,  BT_HST2,  BT_HST3, P2P4G,   _______, _______, _______, SCRSHOT, KC_SCRL, KC_PAUS, KC_UP,    _______,            KC_DEL,
         _______, KC_VOLD,  KC_VOLU,  KC_MUTE, KC_EJCT, _______, BAT_LVL, RGB_TOG, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT,                     _______,
         _______,           _______,  _______, _______, _______, _______, RGB_MOD, RGB_RMOD,KC_END,  KC_PGDN, KC_DOWN,             _______, _______,
-                           _______,  _______,                            OSL(FN),                            _______,  _______),
+                           _______,  _______,                            OSL(FN),                            C(KC_C),  _______),
 
     [WIN_BASE] = LAYOUT_ansi_60(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,    KC_BSLS, KC_GRV,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,            KC_DEL,
         CW_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                      KC_ENT,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT, MO(FN),
-                           KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  KC_RWIN),
+                           KC_LALT,  KC_LWIN,                            KC_SPC,                             KC_RWIN,  KC_RALT),
 
     [FN] = LAYOUT_ansi_60(
         _______, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,    KC_INS,  KC_DEL,
